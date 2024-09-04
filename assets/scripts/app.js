d814237a7d30dae17de931899e47eab8b2ad0d0f
Saman Tavasoli <samsamtava@gmail.com>
2024-09-03 19:30:24 -0700

    090320240730

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,11 +3,13 @@ let currentResult = defaultResult
 
 function add(num1, num2) {
     const result = num1 + num2
+    // alert('The result is ' + result);
+    return result;
 }
 
-currentResult = (currentResult + 10) * 3 / 2 -1;
+currentResult = add(1, 2);
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
 
